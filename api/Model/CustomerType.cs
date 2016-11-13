de72fd23b1e2c1e8a6d2d49deec1ef9c7b0ff266
Carlos Fernandez Jimenez <cfdez89@gmail.com>
2016-11-12 21:57:27 -0600

    enum para tipo de usuario

---
+++ api/Model/CustomerType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace api.Model
+{
+    public enum CustomerType
+    {
+        Standard = 0,
+        Trade = 1
+    }
+}
